8a443df3324fbc5c73da75490c1c70bf9b3f7676
Akanksh Chitimalla <144524423+akanksh-cs50@users.noreply.github.com>
2024-12-02 17:51:18 +0530

    added blank functions to implement and imported neccessary libraries

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, request
+import os
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello World!</p>"
+@app.route("/songs")
+def songs():
+    pass 
+
+@app.route("/artists")
+def songs():
+    pass
+
+@app.route("/albums")
+def songs():
+    pass
+
+@app.route("/search")
+def songs():
+    pass
